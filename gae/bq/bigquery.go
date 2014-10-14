eab11c6df892a4e777bf2b64d8d6a4ffb4b9e382
hiroyuki matsuura <starocean8121@gmail.com>
2014-10-14 09:24:57 +0900

    add PullReport func.

--- gae/bq/bigquery.go
+++ gae/bq/bigquery.go
@@ -1,7 +1,13 @@
 package bq
 
 import (
+	"encoding/json"
 	"time"
+
+	"appengine"
+	"appengine/taskqueue"
+
+	"github.com/knightso/base/errors"
 )
 
 type Task struct {
@@ -9,4 +15,30 @@ type Task struct {
 	InsertID string
 	Time     time.Time
 	Record   interface{}
+}
+
+func PullReport(c appengine.Context, logID, insertID string, jsonReport interface{}) error {
+	v := Task{
+		LogID:    logID,
+		InsertID: insertID,
+		Time:     time.Now(),
+		Record:   jsonReport,
+	}
+
+	payload, err := json.Marshal(v)
+	if err != nil {
+		return errors.WrapOr(err)
+	}
+
+	task := taskqueue.Task{
+		Payload: payload,
+		Method:  "PULL",
+		Tag:     logID,
+	}
+	_, err = taskqueue.Add(c, &task, "log2bigquery")
+	if err != nil {
+		return errors.WrapOr(err)
+	}
+
+	return nil
 }
